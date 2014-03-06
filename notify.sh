e089918e381188e57bb32fbed818e6ab90408079
Tomasz bla Fortuna <bla@thera.be>
2014-03-06 22:01:35 +0100

    Added example, final touches.

---
+++ notify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Notification
+echo "Security breach. Your mom's home!" | mail root@localhost
+
+# You could as well start recording audio here, then encrypt it with gpg and
+# rsync to remote server.
